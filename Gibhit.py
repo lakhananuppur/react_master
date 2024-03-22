02f63b36670a17fd6a0a3bf6ba78e0502bd805de
lakhananuppur <lakhananuppur@gmail.com>
2024-03-22 07:56:37 +0530

    first commit

---
+++ Gibhit.py
@@ -0,0 +1,17 @@
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real, "i +", self.img, "j")
+
+
+num1 = Complex(1, 3)
+num1.showNumber()        
+
+
+
+num1 = Complex(4, 6)
+num1.showNumber()
